52d49733f98e5a829a7239fce5a184362ea11349
Steve Simon <mail@pmean.com>
2020-07-18 19:21:20 -0500

    Added dates to bib files

--- src/step0-identify-missing-fields.R
+++ src/step0-identify-missing-fields.R
@@ -13,19 +13,22 @@ suppressMessages(suppressWarnings(library(rmarkdown)))
 suppressMessages(suppressWarnings(library(stringr  )))
 suppressMessages(suppressWarnings(library(tidyverse)))
 
-year_list <- "20"
+year_list <- c("18", "19", "20")
 for (i_year in year_list) {
-  list.files(path=paste0("text/", i_year), pattern="*.md", full.names=TRUE) -> file_list
+  list.files(path=paste0("text/", i_year), pattern="*.bib", full.names=TRUE) -> file_list
   for (i_file in file_list) {
     file_text <- readLines(i_file)
     if(any(str_detect(file_text, "urldate"))) next
     cat("\n")
     cat(i_file)
-    cat(" ")
+    cat(" ") 
     i_file %>%
       file.info %>%
       pull(ctime) %>% 
       str_sub(1, 10) %>%
-      cat
+      paste0("urldate = {", ., "},") -> new_line
+    cat(new_line)
+    new_text <- c(file_text[1], new_line, file_text[-1])
+    writeLines(new_text, con=i_file)
   }
 }
