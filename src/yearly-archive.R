dca86d158876c36a545640cbb08800748885c265
Steve Simon <mail@pmean.com>
2022-01-02 09:58:25 -0600

    Added yearly-archive.R

---
+++ src/yearly-archive.R
@@ -0,0 +1,54 @@
+# yearly-archive.R. This program was written by Steve Simon on 2021-01-02.
+
+suppressMessages(suppressWarnings(library(base64enc)))
+suppressMessages(suppressWarnings(library(curl     )))
+suppressMessages(suppressWarnings(library(knitr    )))
+suppressMessages(suppressWarnings(library(lubridate)))
+suppressMessages(suppressWarnings(library(magrittr )))
+suppressMessages(suppressWarnings(library(readr    )))
+suppressMessages(suppressWarnings(library(rmarkdown)))
+suppressMessages(suppressWarnings(library(stringr  )))
+suppressMessages(suppressWarnings(library(tidyverse)))
+
+source(file="src/standard_functions.R")
+
+old_path <- "c:/users/steve/dropbox/professional/fun stuff/update-website/"
+new_path <- "text/"
+archive_new <- function(yr) {  
+  new_path %0% 
+    yr %>%
+    list.files %>%
+    str_subset("md$") %>%
+    str_replace(fixed(".Rmd"), "") %>%
+    str_replace(fixed(".md"), "") -> new_files
+  cat("\n\nNew website (")
+  cat(length(new_files))
+  cat(" files)\n\n")
+  "+ [" %0% 
+    new_files %0%
+    "](http://new.pmean.com/" %0%
+    new_files %0%
+    "/)" %>%
+    cat(sep="\n")
+}
+archive_old <- function(yr) {
+  old_path %0% 
+    yr %>%
+    list.files -> old_files
+  cat("\n\nOld website\n\n")
+  "+ [" %0% 
+    old_files %>%
+    str_replace(fixed(".html"), "") %>%
+    str_replace(fixed(".htm"), "") %0%
+    "](http://www.pmean.com/" %0%
+    yr %0%
+    "/" %0%
+    old_files %0%
+    ")" %>%
+    cat(sep="\n")
+}
+
+yrs <- c("2003", "2007", "2011", "2015", "2019", "2021")
+paste0(brack(yrs), paren("http://new.pmean.com/" %0% yrs %0% "/"), collapse=" |\n") %>% cat
+cat("\n\n")
+archive_new("21")
---
+++ text/progress-through-2021.md
@@ -0,0 +1,29 @@
+---
+title: "Progress on my website through the end of 2021"
+source: "New"
+date: "2022-01-02"
+categories:
+- Blog post
+tags:
+- Website details
+output: html_document
+---
+
+I spent much of Christmas break updating files from the year [2007][si07]. This was one of my most productive years. I developed presentations on [children in research][sich], [open access][sioa], quality control, and [post-modernism versus EBM][sipo]. I applied for and received a [promotion][sipr]. I wrote over 160 web pages that year, and it took a long time to update these to the format for the new website.
+
+This is the fifth year that I have updated (other years: [2003][si03], [2011][si11], [2015][si15], [2019][si19]). This is a total of 360 webpages updated. With over 1,700 pages in the old website, I have a lot of work left to do.
+
+At the same time, I have been added 117 new webpages during [2021][si21].
+
+[si03]: http://new.pmean.com/2003/
+[si07]: http://new.pmean.com/2007/
+[si11]: http://new.pmean.com/2011/
+[si15]: http://new.pmean.com/2015/
+[si19]: http://new.pmean.com/2019/
+[si21]: http://new.pmean.com/2021/
+
+[sioa]: http://new.pmean.com/OATalk/
+[sidi]: http://new.pmean.com/DiagnosticTalk/
+[sipo]: http://new.pmean.com/postmodern-assault/
+[sipr]: http://new.pmean.com/Promotion/
+[sich]: http://new.pmean.com/PediatricResearch/
